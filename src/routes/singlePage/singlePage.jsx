41c371dc90cdbcae1217d0084eeebf28886bdf3f
Ulugbek <u.dushanov@gmail.com>
2024-05-08 18:18:50 +0500

    feat: add single page layout

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,3 +1,4 @@
+import Map from "../../components/map/map";
 import Slider from "../../components/slider/slider";
 import { singlePostData, userData } from "../../lib/dummyData";
 import "./singlePage.scss";
@@ -28,7 +29,85 @@ const SinglePage = () => {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="utility-icon" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="pet-icon" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="fee-icon" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total gousehold income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Room Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="size-icon" />
+              <span>80 sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="bed-icon" />
+              <span>2 beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="bath-icon" />
+              <span>1 bathroom</span>
+            </div>
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="school-icon" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="bus-stop-icon" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="restaraunt-icon" />
+              <div className="featureText">
+                <span>Restaraunt</span>
+                <p>200m away</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="chat-icon" />
+              Send a message
+            </button>
+            <button>
+              <img src="/save.png" alt="save-icon" />
+              Save the place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
