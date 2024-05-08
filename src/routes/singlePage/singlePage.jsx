3b7a6f4d2aab54f180c03360c80645b0237b74d8
Ulugbek <u.dushanov@gmail.com>
2024-05-08 16:50:27 +0500

    feat: add slider to single page

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,7 +1,37 @@
+import Slider from "../../components/slider/slider";
+import { singlePostData, userData } from "../../lib/dummyData";
 import "./singlePage.scss";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="location-icon" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="user-img" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
